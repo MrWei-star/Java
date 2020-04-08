29e7488d1785b7a7b7b27618a456ae29356fa375
MrWei-star <53897694+MrWei-star@users.noreply.github.com>
2020-04-08 11:42:31 +0800

    Update CallableDemo.java

--- 尚硅谷-互联网大厂高频重点面试题第2季(上)-思维导图+Demo代码/Demo代码/CallableDemo.java
+++ 尚硅谷-互联网大厂高频重点面试题第2季(上)-思维导图+Demo代码/Demo代码/CallableDemo.java
@@ -24,7 +24,7 @@ public class CallableDemo {
         Thread t1 = new Thread(futureTask,"AA");
         t1.start();
         int r1 = 100;
-        int r2 = futureTask.get();//要求获得Callable线程的计算结果，如果没有计算完成就要强求，会导致阻塞，知道计算完成，建议放在最后
+        int r2 = futureTask.get();//要求获得Callable线程的计算结果，如果没有计算完成就要强求，会导致阻塞，直到计算完成，建议放在最后
 
         System.out.println(".....result"+r1+r2);
     }
